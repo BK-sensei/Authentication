f0b49fc9559759012b16bac02fdebcc8b1bb6563
becca <rebecca.kaci@gmail.com>
2022-01-03 17:29:53 +0100

    création des routes en TypeScript

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Login from './pages/Login'
+import Admin from './pages/Admin'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
